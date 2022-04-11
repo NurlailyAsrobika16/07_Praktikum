3f324c37cf43204394896c04dcde37a68955251f
asrobika01 <asrobika01@gmail.com>
2022-04-11 10:49:08 +0700

    menambah kolom

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -38,6 +38,25 @@
                     <label for="Jurusan">Jurusan</label>
                     <input type="Jurusan" name="Jurusan" class="form-control" id="Jurusan" ariadescribedby="Jurusan">
                 </div>
+                <div class="form-group">
+                    <label for="E-mail">E-mail</label>
+                    <input type="email" name="Email" class="form-control" id="Email" ariadescribedby="Email">
+                </div>
+                <div class="form-group">
+                    <label for="Alamat">Alamat</label>
+                    <input type="text" name="Alamat" class="form-control" id="Alamat" ariadescribedby="Alamat">
+                </div>
+                <div class="form-group">
+                    <label for="Tanggal_lahir">Tanggal Lahir</label>
+                    <input type="date" name="Tanggal_lahir" class="form-control" id="Tanggal_lahir" ariadescribedby="Tanggal_lahir">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
